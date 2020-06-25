fe98640842ee7406093d67ef3bc34b530462b656
Adam Hussain <afymagic@gmail.com>
2020-06-25 18:51:43 +0100

    V1.0.2
    Oled Display Operational

--- include/display.h
+++ include/display.h
@@ -1,33 +1,15 @@
 #ifndef DISPLAY
 #define DISPLAY
 
-#include <heltec.h>
+//Display Header files
+#include "heltec.h"
 #include "OLEDDisplayUi.h"
-#include "xor_logo.h" //xor logo in xbm format
+#include "xor_logo.h" 
 
-
-//Ui
+//Ui initilisation
 extern Heltec_ESP32 Heltec;
 OLEDDisplayUi ui(Heltec.display);
-
-template <class frame> 
-frame displayframe(frame Stat_1,  frame Stat_2, frame Stat_3, frame Stat_4, frame Val_1, frame Val_2, frame Val_3, frame Val_4) 
-{ 
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x, y + 5, Stat_1);
-  display->drawString(x, y + 15, Stat_2);
-  display->drawString(x, y + 25, Stat_3);
-  display->drawString(x, y + 35, Stat_4);
-  display->drawString(x + 55, y + 5, Val_1);
-  display->drawString(x + 55, y + 15, Val_2);
-  display->drawString(x + 55, y + 15, Val_3);
-  display->drawString(x + 55, y + 25, Val_4);
-  display->drawXbm(x + 95, y + 10, 31, 39, logo_bits);
-
- return 0; 
-} 
-
+String lineStatef1[7];
 
 void msOverlay(OLEDDisplay *display, OLEDDisplayUiState *state) //Defines times between frames 
 {
@@ -42,25 +24,20 @@ void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int1
   display->setFont(ArialMT_Plain_16);
   display->drawString(x + 20, y + 10, "XOR");
   display->drawString(x + 20, y + 25, "Systems");
-  display->drawXbm(x + 95, y + 10, 31, 39, logo_bits);
+ // display->drawXbm(x + 95, y + 10, 31, 39, logo_bits); //XOR logo, image needs rectified
 }
-
-void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 2: Sensor Values 
-{
-  //displayframe<std::string,std::string,std::string,std::string,int,int,int,int>("hello", "hello", "hello", "hello", "hello", 1, 2, 3, 4);
-}
-
-void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 3: WiFi/Data Status 
-{
-  //displayframe<std::string,std::string,std::string,std::string,int,int,int,int>("hello", "hello", "hello", "hello", "hello", 1, 2, 3, 4);
-}
-
-void drawFrame4(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 4: Debugging 
-{
-  //displayframe<std::string,std::string,std::string,std::string,int,int,int,int>("hello", "hello", "hello", "hello", "hello", 1, 2, 3, 4);
-}
-
-FrameCallback frames[] = {drawFrame1, drawFrame2, drawFrame3, drawFrame4}; //Frame Array 
-
+ void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) 
+  {
+    display->setTextAlignment(TEXT_ALIGN_LEFT);
+    display->setFont(ArialMT_Plain_10);
+    display->drawString(x, y + 5, lineStatef1[0]);
+    display->drawString(x, y + 15, lineStatef1[2]);
+    display->drawString(x, y + 25, lineStatef1[4]);
+    display->drawString(x, y + 35, lineStatef1[6]);
+    display->drawString(x + 55, y + 5, lineStatef1[1]);
+    display->drawString(x + 55, y + 15, lineStatef1[3]);
+    display->drawString(x + 55, y + 15, lineStatef1[5]);
+    display->drawString(x + 55, y + 25, lineStatef1[7]);
+  }
 
 #endif //DISPLAY
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include "display.h"
 
+#define framecount 2
+
+FrameCallback frames[] = {drawFrame1,drawFrame2}; //Frame Array 
+
 void setup()
 {
   
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, false /*Serial Enable*/); //Oled Enable
-  
   ui.setTargetFPS(30); //Refresh Rate 
   ui.init(); //Initialise 
   ui.setIndicatorPosition(BOTTOM); //Indicator = Framecallback Status 
   ui.setIndicatorDirection(LEFT_RIGHT); 
   ui.setFrameAnimation(SLIDE_LEFT);
-  ui.setFrames(frames, 4); 
-  
+  ui.setFrames(frames, framecount); 
   Heltec.display->flipScreenVertically();
 }
+
 
 void loop()
 {
     ui.update(); //Update Screen 
+
+
 }
