a069b8a29434e366ae927045b7698f55ac701c62
Adam Hussain <afymagic@gmail.com>
2020-06-25 06:03:19 +0100

    V1.0.1
    Xor Logo

--- lib/display.h
+++ lib/display.h
@@ -1,20 +1,30 @@
 #ifndef DISPLAY
 #define DISPLAY
 
-#include "Arduino.h"
 #include <heltec.h>
-#include <OLEDDisplayUi.h>
+#include "OLEDDisplayUi.h"
+
 
 //Ui
 extern Heltec_ESP32 Heltec;
 OLEDDisplayUi ui(Heltec.display);
 
-template <typename frame> 
+template <class frame> 
 frame displayframe(frame Stat_1,  frame Stat_2, frame Stat_3, frame Stat_4, frame Val_1, frame Val_2, frame Val_3, frame Val_4) 
 { 
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(x, y + 5, Stat_1);
+  display->drawString(x, y + 15, Stat_2);
+  display->drawString(x, y + 25, Stat_3);
+  display->drawString(x, y + 35, Stat_4);
+  display->drawString(x + 55, y + 5, Val_1);
+  display->drawString(x + 55, y + 15, Val_2);
+  display->drawString(x + 55, y + 15, Val_3);
+  display->drawString(x + 55, y + 25, Val_4);
+  display->drawXbm(x + 95, y + 10, 31, 39, hhlogo_bits);
 
-
-   return 0; 
+ return 0; 
 } 
 
 
@@ -36,42 +46,18 @@ void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int1
 
 void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 2: Sensor Values 
 {
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x, y + 5, "Light (Lx):");
-  display->drawString(x + 51, y + 5, SLux);
-  display->drawString(x, y + 15, "Temp (C):");
-  display->drawString(x + 51, y + 15, Stemp);
-  display->drawString(x, y + 25, "RH (%):");
-  display->drawString(x + 51, y + 25, Shumidity);
-  display->drawString(x, y + 35, "C02 (ppm):");
-  display->drawString(x + 51, y + 35, SC02);
-  display->drawXbm(x + 95, y + 10, 31, 39, hhlogo_bits);
+  displayframe<std::string,std::string,std::string,std::string,int,int,int,int>("hello", "hello", "hello", "hello", "hello", 1, 2, 3, 4);
+
 }
 
 void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 3: WiFi/Data Status 
 {
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x, y + 5, Wi_fi_Output);
-  display->drawString(x, y + 15, ssid);
-  display->drawString(x, y + 25, Data_Output);
-  display->drawXbm(x + 95, y + 10, 31, 39, hhlogo_bits);
+
 }
 
 void drawFrame4(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) //Frame 4: Debugging 
 {
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x, y + 5, "WiFi_Poll:");
-  display->drawString(x, y + 15, "Data_Poll:");
-  display->drawString(x, y + 25, "TS_Poll:");
-  display->drawString(x, y + 35, "TS Code:");
-  display->drawString(x + 55, y + 5, SWiFi_Poll);
-  display->drawString(x + 55, y + 15, SSensor_Poll);
-  display->drawString(x + 55, y + 15, STS_Poll);
-  display->drawString(x + 55, y + 25, STS_Status);
-  display->drawXbm(x + 95, y + 10, 31, 39, hhlogo_bits);
+
 }
 
 FrameCallback frames[] = {drawFrame1, drawFrame2, drawFrame3, drawFrame4}; //Frame Array 
